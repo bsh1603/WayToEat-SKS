538a93f4e8f6a3328362d2012f45c07c96587885
bsh1603 <hyun960303@naver.com>
2023-02-12 20:29:50 +0900

    chore: backend init

---
+++ src/main/java/bkk/waytoeat/WaytoeatApplication.java
@@ -0,0 +1,13 @@
+package bkk.waytoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaytoeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaytoeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bkk/waytoeat/WaytoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package bkk.waytoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaytoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
