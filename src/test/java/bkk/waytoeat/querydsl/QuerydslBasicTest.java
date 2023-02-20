fa58e8592a5ac4f1f762126ab3503d26978e3b52
bsh1603 <hyun960303@naver.com>
2023-02-21 00:59:44 +0900

    test: querydsl 설정 테스트

---
+++ src/test/java/bkk/waytoeat/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,40 @@
+package bkk.waytoeat.querydsl;
+
+import bkk.waytoeat.domain.querydsltest.Hello;
+import bkk.waytoeat.domain.querydsltest.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @Test
+    void 기본테스트() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        queryFactory = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = queryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+}
